383c7e242239bb0f7cd6251efe233fa6510cba3f
xuedishangdemeigui <2216336843@qq.com>
2023-01-25 11:53:40 +0800

    创建环境

--- src/main/java/com/zc/http/App.java
+++ src/main/java/com/zc/http/App.java
@@ -2,9 +2,11 @@ package com.zc.http;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.io.Reader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -29,15 +31,23 @@ public class App {
 
     private static void socketHandler(Socket socket) {
 
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        try (
+                // InputStream is = socket.getInputStream();
+                // Reader reader = new InputStreamReader(is);
+                // BufferedReader br = new BufferedReader(reader);
+                BufferedReader br =
+                        new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter pw = new PrintWriter(
                         new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));) {
 
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = br.readLine()) != null && line.length() != 0) {
                 System.out.println(line);
             }
             System.out.println("read finished");
+
+            // parseRequest(is);
+            System.out.println(socket.isClosed());
 
             pw.write("HTTP/1.1 200 \r\n");
             // pw.write("Transfer-Encoding: chunked");
@@ -50,7 +60,22 @@ public class App {
         }
     }
 
-    // private static void parseRequest(Socket socket) {
-    //     BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-    // }
+    private static void parseRequest(InputStream inputStream) {
+        try (
+                //
+                Reader reader = new InputStreamReader(inputStream);
+                BufferedReader br = new BufferedReader(reader);
+        // BufferedReader br =
+        // new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        ) {
+            String line;
+            while ((line = br.readLine()) != null && line.length() != 0) {
+                System.out.println(line);
+            }
+            System.out.println("read finished");
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+    }
 }
